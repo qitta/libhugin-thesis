9037681a48553adc4c88173b2f3b5a51e5ae85e2
kitteh <christoph@nullcat.de>
2014-05-09 10:57:30 +0200

    catlight build script added.

--- autobuild.sh
+++ autobuild.sh
@@ -1,5 +1,7 @@
 while true;
 do
   inotifywait -e close_write -r ./rst conf.py *.rst *.bib *.py
-  make latexpdf
+  catlight rgb 255 50 0
+  make clean && make latexpdf
+  catlight rgb 0 255 0
 done
