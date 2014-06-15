0979158566ac6e9fa2fbd9f8c274d2a67a67781b
qitta <christoph@nullcat.de>
2014-06-15 18:41:51 +0200

    Cleaned httplib_test.py

--- rst/attachments/httplib_test.py
+++ rst/attachments/httplib_test.py
@@ -4,6 +4,7 @@ import urllib.request
 import time
 import urllib3
 import httplib2
+import pylab
 
 URLS = [
     'http://www.zeit.de',
@@ -18,69 +19,63 @@ URLS = [
     'http://www.spiegel.de',
     'http://www.zeit.de',
     'http://www.faz.de'
-        ]
+]
 
+def fetch_urllib(url, timeout):
+    return urllib.request.urlopen(url, timeout=timeout).readall()
 
-def load_url1(url, timeout):
-    c = urllib.request.urlopen(url, timeout=timeout)
-    return c.readall()
-
-def load_url2(url, timeout):
+def fetch_httplib2(url, timeout):
     h, c = httplib2.Http().request(url)
     return c
 
 PM = urllib3.PoolManager()
-def load_url3(url, timeout):
+
+def fetch_urllib3(url, timeout):
     return PM.request(url=url, method='GET').data
 
+FUNCS = {
+    'urllib': fetch_urllib,
+    'httplib2': fetch_httplib2,
+    'urllib3': fetch_urllib3
+}
+
 def download(threads=1, func=None):
+    start = int(round(time.time() * 1000))
+
     with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
         future_to_url = {executor.submit(func, url, 60): url for url in URLS}
         for future in concurrent.futures.as_completed(future_to_url):
-            url = future_to_url[future]
             try:
-                data = future.result()
-            except Exception as exc:
+                future.result()
+            except Exception:
                 pass
-                #print(exc)
 
+    end = int(round(time.time() * 1000))
+    return end - start
 
-if __name__ == '__main__':
-
-    funcs = {
-        'urllib': load_url1,
-        'httplib2': load_url2,
-        'urllib3' : load_url3
-    }
-    results = defaultdict(dict)
-    for name, func in funcs.items():
-        run_results = []
-        for thread in range(1, 10+1, 1):
-            avg_time = 0
-            for run in range(1):
-                start = int(round(time.time() * 1000))
-                download(threads=thread, func=func)
-                end = int(round(time.time() * 1000))
-                avg_time += end - start
-            run_results.append( (thread, avg_time) )
-        results[name] = run_results
-        run_results = []
-    import pprint
-    pprint.pprint(results)
-
-    from pylab import *
-
-    for lib in funcs.keys():
+def plot(results):
+    for lib in FUNCS.keys():
         t = [x[0] for x in results[lib]]
         s = [x[1] for x in results[lib]]
-        plot(t, s, label=lib)
+        pylab.plot(t, s, label=lib)
 
-    xlim(1,10)  # decreasing time
+    pylab.xlim(1, 10)  # decreasing time
+    pylab.xlabel('number of download threads')
+    pylab.ylabel('time in milliseconds')
+    pylab.title('performance comparsion threaded download')
+    pylab.grid(True)
+    pylab.legend()
+    pylab.show()
 
-    xlabel('number of download threads')
-    ylabel('time in milliseconds')
-    title('performance comparsion threaded download')
-    grid(True)
-    legend()
+if __name__ == '__main__':
+    N = 10
+    results = defaultdict(dict)
 
-    show()
+    for name, func in FUNCS.items():
+        run_results = []
+        for threads in range(1, 10 + 1, 1):
+            avg_time = sum(download(threads=threads, func=func) for _ in range(N))
+            run_results.append((threads, avg_time))
+        results[name] = run_results
+
+    plot(results)
