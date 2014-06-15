2228cff6fabb5d78d863bbbc3a3d6ff126f533f3
qitta <christoph@nullcat.de>
2014-06-15 22:55:59 +0200

    Cleaned rating.py

--- rst/attachments/rating.py
+++ rst/attachments/rating.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
+# encoding: utf-8
 
 import numpy as np
 import matplotlib.pyplot as plt
-from collections import Counter
 from collections import Counter
 from statistics import mean
 import pprint
@@ -10,9 +10,7 @@ import sys
 import os
 import json
 
-
 PROVIDERS = ['tmdb', 'ofdb', 'omdb']
-
 
 def analyze_folder(path):
     data = {}
@@ -22,25 +20,24 @@ def analyze_folder(path):
             data[provider] = json.loads(fp.read())
     return data
 
-
 def analyze():
-    results = {p: list() for p in PROVIDERS}
-    for folder in [f for f in os.listdir(sys.argv[1]) if os.path.isdir(f)]:
-        providers = analyze_folder(folder)
-        for name, metadata in providers.items():
-            if name in PROVIDERS:
-                rating = metadata.get('rating')
-                if rating:
-                    results[name].append(float(rating))
-    return results
+    results = {p: [] for p in PROVIDERS}
+    for folder in filter(os.path.isdir, os.listdir(sys.argv[1])):
+        for name, metadata in analyze_folder(folder).items():
+            if name not in PROVIDERS:
+                continue
 
+            rating = metadata.get('rating')
+            if rating:
+                results[name].append(float(rating))
+
+    return results
 
 def calculate_min_mean_max(data):
     rating_stats = {name: Counter() for name in PROVIDERS}
     for name, ratings in data.items():
-        rating_stats[name] = ( min(ratings), round(mean(ratings), 2), max(ratings))
+        rating_stats[name] = (min(ratings), round(mean(ratings), 2), max(ratings))
     return rating_stats
-
 
 def count_ratings(data):
     rating_counts = {name: Counter() for name in PROVIDERS}
@@ -49,14 +46,13 @@ def count_ratings(data):
             rating_counts[name][value] += 1
     return rating_counts
 
-
 def plot_distribution(data):
     colors = ['r', 'g', 'b']
     _, axes = plt.subplots(len(data), sharex=True, sharey=True)
     axes = list(axes)
 
     axes[0].set_ylabel('Number of given ratings')
-    #axes[0].set_xlabel('Rating')
+    axes[0].set_xlabel('Rating')
 
     for idx, provider in enumerate(data.keys()):
         ax = axes.pop()
@@ -70,15 +66,14 @@ def plot_distribution(data):
         X = np.array(list(ratings.keys()))
         Y = np.array(list(ratings.values()))
 
-        width = 0.4
-        rects1 = ax.bar(X, Y, width, color=colors.pop())
+        ax.bar(X, Y, 0.4, color=colors.pop())
         ax.set_title('Rating Distribution of "{p}"'.format(p=provider))
         ax.set_xticks(X)
+        ax.grid(True)
 
     plt.show()
 
 if __name__ == '__main__':
     data = analyze()
-    counted_data = count_ratings(data)
-    plot_distribution(counted_data)
+    plot_distribution(count_ratings(data))
     pprint.pprint(calculate_min_mean_max(data))
